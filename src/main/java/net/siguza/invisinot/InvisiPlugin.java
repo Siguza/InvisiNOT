6dc5fe509ad8b001cbdbd0537472ffdcd8076f70
Siguza <molt@drgnome.net>
2014-03-30 17:37:04 +0200

    Finished name prefix, added reload command

--- src/main/java/net/siguza/invisinot/InvisiPlugin.java
+++ src/main/java/net/siguza/invisinot/InvisiPlugin.java
@@ -6,9 +6,11 @@
 package net.siguza.invisinot;
 
 import java.util.logging.Logger;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
+import org.bukkit.command.*;
 import org.bukkit.event.*;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.entity.Player;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -34,6 +36,7 @@ public class InvisiPlugin extends JavaPlugin implements Runnable, Listener
     public void onEnable()
     {
         _log.info("Enabling InvisiNOT " + _version);
+        saveDefaultConfig();
         MetaListener.register(getConfig().getString("name-prefix"));
         if(getConfig().getString("check-update").equalsIgnoreCase("true"))
         {
@@ -49,7 +52,48 @@ public class InvisiPlugin extends JavaPlugin implements Runnable, Listener
         {
             getServer().getScheduler().cancelTasks(this);
         }
+        HandlerList.unregisterAll((Plugin)this);
         _log.info("Disabled InvisiNOT " + _version);
+    }
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if((args.length <= 0) || args[0].equalsIgnoreCase("help"))
+        {
+            sender.sendMessage(ChatColor.AQUA + "/invisinot version - Shows the current version");
+            if(sender.hasPermission("invisinot.admin"))
+            {
+                sender.sendMessage(ChatColor.AQUA + "/invisinot reload - Reload the config");
+            }
+            return true;
+        }
+        if(args[0].equalsIgnoreCase("version"))
+        {
+            sender.sendMessage(ChatColor.GREEN + "InvisiNOT version: " + _version);
+        }
+        else if(args[0].equalsIgnoreCase("reload"))
+        {
+            if(!sender.hasPermission("invisinot.admin"))
+            {
+                sender.sendMessage(ChatColor.RED + "You're not allowed to use this command!");
+                return true;
+            }
+            MetaListener.unregister();
+            if(!_update)
+            {
+                getServer().getScheduler().cancelTasks(this);
+            }
+            HandlerList.unregisterAll((Plugin)this);
+            reloadConfig();
+            MetaListener.register(getConfig().getString("name-prefix"));
+            if(getConfig().getString("check-update").equalsIgnoreCase("true"))
+            {
+                getServer().getScheduler().scheduleSyncRepeatingTask(this, this, 0L, 72000L);
+                getServer().getPluginManager().registerEvents(this, this);
+            }
+            sender.sendMessage(ChatColor.GREEN + "InvisiNOT config reloaded.");
+        }
+        return true;
     }
     
     @EventHandler(priority = EventPriority.MONITOR)
--- src/main/java/net/siguza/invisinot/MetaListener.java
+++ src/main/java/net/siguza/invisinot/MetaListener.java
@@ -5,23 +5,37 @@
 
 package net.siguza.invisinot;
 
+import java.lang.reflect.*;
 import java.util.*;
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
+import net.minecraft.util.com.mojang.authlib.GameProfile;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.*;
+import com.comphenix.protocol.*;
 import com.comphenix.protocol.events.*;
 import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.wrappers.*;
 
 public class MetaListener extends PacketAdapter
 {
-    private final boolean _alterName;
-    private final String _namePrefix;
-
+    private static boolean _alterName;
+    private static String _namePrefix;
+    private static HashMap<Integer, Boolean> _stateCache;
+    
     public static void register(String namePrefix)
     {
+        _namePrefix = namePrefix.replace("&", new String(new char[]{ChatColor.COLOR_CHAR}));
+        _alterName = namePrefix.length() > 0;
+        _stateCache = _alterName ? new HashMap<Integer, Boolean>() : null;
         ArrayList<PacketType> list = new ArrayList<PacketType>();
         list.add(PacketType.Play.Server.ENTITY_METADATA);
-        ProtocolLibrary.getProtocolManager().addPacketListener(new MetaListener(list, namePrefix));
+        ProtocolLibrary.getProtocolManager().addPacketListener(new MetaListener(list));
+        if(_alterName)
+        {
+            list = new ArrayList<PacketType>();
+            list.add(PacketType.Play.Server.NAMED_ENTITY_SPAWN);
+            ProtocolLibrary.getProtocolManager().addPacketListener(new PlayerListener(list));
+        }
     }
     
     public static void unregister()
@@ -29,16 +43,39 @@ public class MetaListener extends PacketAdapter
         ProtocolLibrary.getProtocolManager().removePacketListeners(InvisiPlugin.instance());
     }
     
-    public MetaListener(Iterable<PacketType> list, String namePrefix)
+    private static boolean isPlayer(int entityID)
+    {
+        for(Player p : Bukkit.getOnlinePlayers())
+        {
+            if(p.getEntityId() == entityID)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private static Player getPlayer(int entityID)
+    {
+        for(Player p : Bukkit.getOnlinePlayers())
+        {
+            if(p.getEntityId() == entityID)
+            {
+                return p;
+            }
+        }
+        return null;
+    }
+    
+    public MetaListener(Iterable<PacketType> list)
     {
         super(InvisiPlugin.instance(), ListenerPriority.LOWEST, list);
-        _namePrefix = namePrefix;
-        _alterName = namePrefix.length() > 0;
     }
     
     public void onPacketSending(PacketEvent event)
     {
-        if(!event.getPlayer().hasPermission("invisinot.see"))
+        Player player = event.getPlayer();
+        if(!player.hasPermission("invisinot.see"))
         {
             return;
         }
@@ -46,27 +83,17 @@ public class MetaListener extends PacketAdapter
         {
             PacketContainer packet = event.getPacket().deepClone();
             StructureModifier<Object> mod = packet.getModifier();
-            int num = 0;
-            List list = null;
-            if(mod != null)
-            {
-                for(int i = 0; i < mod.size(); i++)
-                {
-                    Object o = mod.readSafely(i);
-                    System.out.println(i + ": " + o.getClass().getName());
-                    if(o instanceof List)
-                    {
-                        num = i;
-                        list = (List)o;
-                        break;
-                    }
-                }
-            }
-            if(list == null)
+            if(mod == null)
             {
                 InvisiPlugin._log.warning("[InvisiNOT] Unable to edit outgoing packages.");
                 return;
             }
+            int entityID = (Integer)mod.readSafely(0);
+            if(entityID == player.getEntityId())
+            {
+                return;
+            }
+            List list = (List)mod.readSafely(1);
             byte modifiers = 0;
             for(int i = 0; i < list.size(); i++)
             {
@@ -76,16 +103,62 @@ public class MetaListener extends PacketAdapter
                     modifiers = (Byte)data.getValue();
                 }
             }
-            if((modifiers & 0x20) == 0)
+            boolean isPlayer = isPlayer(entityID);
+            boolean isVisible = (modifiers & 0x20) == 0;
+            if(isPlayer && _alterName)
+            {
+                if(_stateCache.containsKey(entityID) && (_stateCache.get(entityID) ^ isVisible))
+                {
+                    _stateCache.put(entityID, isVisible);
+                    ArrayList<Player> plist = new ArrayList<Player>();
+                    plist.add(player);
+                    ProtocolLibrary.getProtocolManager().updateEntity(getPlayer(entityID), plist);
+                    /*ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+                    Player human = getPlayer(entityID);
+                    try
+                    {
+                        manager.sendServerPacket(player, manager.createPacketConstructor(PacketType.Play.Server.ENTITY_DESTROY, human).createPacket(human), true);
+                        manager.sendServerPacket(player, manager.createPacketConstructor(PacketType.Play.Server.NAMED_ENTITY_SPAWN, human).createPacket(human), true);
+                        event.setCancelled(true);
+                        return;
+                    }
+                    catch(IllegalArgumentException e)
+                    {
+                        e.printStackTrace();
+                    }*/
+                }
+                else
+                {
+                    _stateCache.put(entityID, isVisible);
+                }
+            }
+            if(isVisible)
             {
                 return;
             }
-            modifiers &= 0xdf;
             ArrayList<Object> newlist = new ArrayList<Object>();
-            newlist.add(new WrappedWatchableObject(0, modifiers).getHandle());
-            if(_alterName)
+            newlist.add(new WrappedWatchableObject(0, (byte)(modifiers & 0xdf)).getHandle());
+            if(_alterName && !isPlayer)
             {
-                newlist.add(new WrappedWatchableObject(10, _namePrefix + "Placeholder").getHandle());
+                String name = (String)mod.readSafely(10);
+                if((name == null) || (name.length() == 0))
+                {
+                    try
+                    {
+                        Entity e = ProtocolLibrary.getProtocolManager().getEntityFromID(player.getWorld(), entityID);
+                        Method m = e.getClass().getMethod("getHandle");
+                        m.setAccessible(true);
+                        Object o = m.invoke(e);
+                        m = o.getClass().getMethod("getName");
+                        m.setAccessible(true);
+                        name = (String)m.invoke(o);
+                    }
+                    catch(Throwable t)
+                    {
+                        name = "generic";
+                    }
+                }
+                newlist.add(new WrappedWatchableObject(10, _namePrefix + name).getHandle());
                 newlist.add(new WrappedWatchableObject(11, (byte)1).getHandle());
             }
             for(int i = 0; i < list.size(); i++)
@@ -98,7 +171,7 @@ public class MetaListener extends PacketAdapter
                         break;
                     case 10:
                     case 11:
-                        if(_alterName)
+                        if(_alterName && !isPlayer)
                         {
                             break;
                         }
@@ -107,12 +180,51 @@ public class MetaListener extends PacketAdapter
                         break;
                 }
             }
-            mod.writeSafely(num, newlist);
+            mod.writeSafely(1, newlist);
             event.setPacket(packet);
         }
         catch(Throwable t)
         {
             t.printStackTrace();
+        }
+    }
+    
+    public static class PlayerListener extends PacketAdapter
+    {
+        public PlayerListener(Iterable<PacketType> list)
+        {
+            super(InvisiPlugin.instance(), ListenerPriority.LOWEST, list);
+        }
+        
+        public void onPacketSending(PacketEvent event)
+        {
+            Player player = event.getPlayer();
+            if(!player.hasPermission("invisinot.see"))
+            {
+                return;
+            }
+            try
+            {
+                PacketContainer packet = event.getPacket().deepClone();
+                StructureModifier<Object> mod = packet.getModifier();
+                if(mod == null)
+                {
+                    InvisiPlugin._log.warning("[InvisiNOT] Unable to edit outgoing packages.");
+                    return;
+                }
+                int entityID = (Integer)mod.readSafely(0);
+                if((entityID == player.getEntityId()) || !_stateCache.containsKey(entityID) || _stateCache.get(entityID))
+                {
+                    return;
+                }
+                GameProfile gp = (GameProfile)mod.readSafely(1);
+                mod.writeSafely(1, new GameProfile(gp.getId(), _namePrefix + gp.getName()));
+                event.setPacket(packet);
+            }
+            catch(Throwable t)
+            {
+                t.printStackTrace();
+            }
         }
     }
 }
